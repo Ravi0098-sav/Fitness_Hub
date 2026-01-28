5ca0760bb0ff9a30cc3150884c77721d4b66ecea
JenishGohel <jenishgohel09@gmail.com>
2026-01-28 10:32:00 +0530

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,98 @@
+@extends('frontend.layouts.main')
+
+@section('main-content')
+
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5">
+    <div class="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style="min-height: 400px">
+        <h4 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase font-weight-bold">Contact Us</h4>
+        <div class="d-inline-flex">
+            <p class="m-0 text-white"><a class="text-white" href="">Home</a></p>
+            <p class="m-0 text-white px-2">/</p>
+            <p class="m-0 text-white">Contact Us</p>
+        </div>
+    </div>
+</div>
+<!-- Page Header End -->
+
+<!-- Contact Start -->
+<div class="container pt-5">
+    <div class="d-flex flex-column text-center mb-5">
+        <h4 class="text-primary font-weight-bold">Get In Touch</h4>
+        <h4 class="display-4 font-weight-bold">Email Us For Any Query</h4>
+    </div>
+
+    <!-- Contact Info -->
+    <div class="row px-3 pb-2">
+        <div class="col-sm-4 text-center mb-3">
+            <i class="fa fa-2x fa-map-marker-alt mb-3 text-primary"></i>
+            <h4 class="font-weight-bold">Address</h4>
+            <p>123 Street, New York, USA</p>
+        </div>
+        <div class="col-sm-4 text-center mb-3">
+            <i class="fa fa-2x fa-phone-alt mb-3 text-primary"></i>
+            <h4 class="font-weight-bold">Phone</h4>
+            <p>+012 345 6789</p>
+        </div>
+        <div class="col-sm-4 text-center mb-3">
+            <i class="far fa-2x fa-envelope mb-3 text-primary"></i>
+            <h4 class="font-weight-bold">Email</h4>
+            <p>info@example.com</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <!-- Google Map -->
+        <div class="col-md-6 pb-5">
+            <iframe style="width: 100%; height: 392px;" 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" 
+                    frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+
+        <!-- Contact Form -->
+        <div class="col-md-6 pb-5">
+            <div class="contact-form">
+                <!-- Success Message -->
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                <form action="{{ route('contact.store') }}" method="POST">
+                    @csrf
+                    <div class="control-group mb-3">
+                        <input type="text" class="form-control" name="name" placeholder="Your Name" required />
+                    </div>
+                    <div class="control-group mb-3">
+                        <input type="email" class="form-control" name="email" placeholder="Your Email" required />
+                    </div>
+                    <div class="control-group mb-3">
+                        <input type="text" class="form-control" name="subject" placeholder="Subject" required />
+                    </div>
+                    <div class="control-group mb-3">
+                        <textarea class="form-control" rows="6" name="message" placeholder="Message" required></textarea>
+                    </div>
+                    <div>
+                        <button class="btn btn-outline-primary" type="submit">Send Message</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Contact End -->
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-outline-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
+
+@endsection
